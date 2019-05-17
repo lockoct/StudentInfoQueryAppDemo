5249a4c85402b8b489430291d10743348c0f862c
1340402575 <1340402575@qq.com>
2019-05-17 20:43:19 +0800

    增加了主页面以及相关图片

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
 @Component({
-  selector: 'app-tab2',
+  selector: 'app-tab2,slides-examle',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+  // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+  @ViewChild('tab2') tab2;
+
+  slideOpts = {
+    // effect: 'coverflow',  //轮播效果
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true
+  };
+  constructor() { }
+  ngOnInit() {
+  }
+
+  //手动滑动后轮播图不自动轮播的解决方法
+  slideDidChange() {
+    this.tab2.startAutoplay();
+  }
+}
